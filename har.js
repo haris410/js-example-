00505cb0bee6ea18ce2d311ccd01b068eef8e32d
haris <harismohammed216@gmail.com>
2024-05-28 00:09:14 +0530

    first commit

---
+++ har.js
@@ -0,0 +1,753 @@
+// console.log("hariss");
+// let age=14
+// if (age>=18) {
+//     console.log("you can vote");
+// }
+// if (age<18){
+// console.log("you cannot vote");
+// }
+
+
+
+
+// let color="black";
+// if (color==="red") {
+//     console.log("stop");
+// }
+// if (color==="yello"){
+//     console.log("start");
+// }
+// if  (color==="green"){
+//     console.log("go");
+//     }
+// else{
+//     console.log("display not working");
+
+//     }
+
+
+
+// console.log("hariss");
+// let age=19
+// if (age>=18) {
+//     console.log("you can vote");
+// }
+// else if (age>=18){
+// console.log("you can vote");
+// }
+// else if (age<18){
+//     console.log("you cannnot vote");
+//     }
+
+
+
+// let marks=(64);
+// if (marks>=85){
+//     console.log("A++");
+// }
+//  else if (marks>=65){
+//     console.log("A");
+// }
+// else if (marks>=35){
+//     console.log("B");
+// }
+// else if (marks<35){
+//     console.log("F");
+// }
+
+
+
+// let age=19;
+// if (age>=18){
+//   console.log("you can votr");
+// }
+// else{
+//     console.log("you cannot vote");
+//  }
+
+
+// let size=("L");
+// if (size==="XL"){
+//     console.log("rs.200");
+// }
+//  else if (size==="L"){
+//     console.log("rs.150");
+// }
+// else if (size==="M"){
+//     console.log("rs.100");
+// }
+// else {
+//     console.log("rs.50");
+
+
+
+
+// --------------------------------------nested if-else statement---------------------------
+// let marks=5;
+// if (marks>=45){
+//     console.log("pass");
+//     if (marks>=85){
+//         console.log("top");
+//     }
+//     else {
+//     console.log("A");    
+//     } 
+// }
+// else{
+//     console.log("try next time");
+// }
+
+
+
+//  ------------------------------------------&& logical AND operater-----------------------
+// let marks = 45;
+// if (marks >=33 && marks >= 80) {
+//     console.log("pass");
+//     console.log("A+");
+// }
+
+
+// ------------------------------------- || logical OR operater-------------------------
+// let marks = 45;
+
+// if (marks >=33 || marks >= 80) {
+//     console.log("pass");
+//     console.log("A+");
+// }
+
+// -----------------------------------------good & bad string---------------------------
+// let str="apple"
+// if (str[0] ==="a" && str.length>3){
+//     console.log("good stirng");
+// }
+// else{
+//     console.log("bad string");
+// }
+
+
+// let num=12;
+
+// if ((num %3 === 0) && ((num+1===15) || (num -1 === 11))){
+//     console.log("safe");
+// }
+// else{
+//     console.log("unsafe");
+// }
+
+
+
+
+// -----------------------------------true and false value----------------------
+
+// if(true){
+//     console.log("this is ture");
+// }
+// else{
+//     console.log("this is false");
+// }
+
+// let (null)
+// if(null){
+//     console.log("this is ture");
+// }
+// else{
+//     console.log("this is false");
+// }
+
+
+
+// -----------------------------------switch statement--------------------------
+// let color ="green";
+// switch(color){
+//     case"red":
+//         console.log("stop");
+//         break;
+//     case"yellow":
+//         console.log("start");
+//         break;
+//     case"green":
+//         console.log("let's go");
+//         break;
+//     default:
+//         console.log("broken light");    
+// }
+
+// let day ="9";
+// switch(day){
+//     case"1":
+//         console.log("monday");
+//         break;
+//     case"2":
+//         console.log("thuesday");
+//         break;
+//     case"3":
+//         console.log("wedensday");
+//         break;
+//     case"4":
+//         console.log("thursday");
+//         break;
+//     case"5":
+//         console.log("friday");
+//         break;
+//     case"6":
+//         console.log("saturday");
+//         break;
+//     case"7":
+//         console.log("sunday");
+//         break;
+//     default:
+//         console.log("There is only seven days in a week")    
+    
+// }
+
+//---------------------------------------------alert-------------------------------
+// alert("Please enter your email and password")
+// console.log("Please enter your email")
+
+
+// --------------------------------------------error---------------------------
+// console.error("This page is unavailable")
+
+
+//  --------------------------------------warn------------------------------------ 
+// console.warn("This is last warning")
+
+
+// ----------------------------------------------prompt---------------------------------
+// let mail = prompt("Please enter your email");
+//     console.log(mail);
+
+
+// let first= prompt("Please enter your firtsname");
+// let last= prompt("Please enter your lastname");
+// console.log("welcome" ,first , last);
+
+// let first= prompt("Please enter your firtsname");
+//  let last= prompt("Please enter your lastname"); 
+// let msg= "welcome"+" "+ first+" "+last ;
+// alert(msg);
+
+
+
+// -------------------------------assignments.-------------------------
+
+//--------modulo  RO  remainder-------------------
+// let num=30;
+
+// if(num % 10== 0) {
+//     console.log("good");
+// }
+// else{
+//     console.log("bad");
+// }
+
+
+
+
+// let nam=prompt("name");
+// let age=prompt("age");
+// alert=(`${nam} is ${age} year old`);
+
+
+
+// let month=("9");
+// switch(month){
+//     case"1":
+//         console.log("jan,feb,march");
+//         break
+//     case"2":
+//         console.log("april,may,jun");
+//         break
+//     case"3":
+//         console.log("jul,aug,sep");
+//         break
+//     case"4":
+//         console.log("oct,nov,dec");
+//         break
+//     default:
+//         console.log("unknown");
+// }
+
+
+
+
+// let str="apple"
+// if( str[0]==="a" && str.length==5){
+//     console.log("good stirng");
+// }
+// else{
+//     console.log("bad string");
+// }
+
+// let a=12;
+// let b=56;
+// let c=34;
+
+// if (a>b){
+//     if (a>c){
+//     console.log("a, is largest"); 
+//     }
+// else{
+//     console.log("c, is largest");  
+// }
+// }
+// else if (b>c) {
+//     console.log("b, is largest");  
+// }
+// else {
+//     console.log("c, is largest"); 
+// }
+
+
+// let num=32
+// let num1=47852;
+
+// if ((num%10)==(num1%10)) {
+//     console.log("yess",num%10);
+// }
+// else{
+//     console.log("no");
+// }
+
+
+
+//------------------------------- indexof-----------------------------------------
+// let str="ILoveCoding";
+// str.indexOf("Coding")
+// str.indexOf("4")
+
+// ------------------method chaining is a----------- useing multiple method -------------- 
+// let str=("     hariis  ")
+// let newstr=str.trim().toUpperCase();
+// console.log(newstr);
+
+// ---------------------------slice OR cut-------------------------- 
+// let mgs="mohammed"
+// console.log(mgs.slice(0,4));
+
+// let mes="mohammed haris"
+// console.log(mes.slice(5,12));
+
+// let mee="mohammed haris"
+// console.log(mee.slice(5));
+
+
+// ----------------------replace--------------
+// let mes="haris mohammed"
+// console.log(mes.replace("haris","abdul"));
+
+// let re="haris "
+// console.log(re.replace("h","W"));
+
+
+// let er="haris haris haris"
+// console.log(re.replace("haris","abdul"));
+
+
+// ----------------------------------repeat-----------------------------------------
+// let rep="haris";
+// console.log(rep.repeat(3));
+
+
+// -------------------------------practices  Q----------------------------
+
+// let sr = "     haris    ";
+// console.log(sr.trim() .toUpperCase());
+
+
+// let sr = "apnacollege";
+// console.log(sr.slice(4,9));
+// console.log(sr.indexOf("na"))
+// console.log(sr.replace("apna","our"))
+// console.log(sr.slice(4).replace("l","t").replace("l","t"))
+
+// ---------------------------------This is ARRAY--------------------------------
+// let students =["haris","majeed","saif"];
+
+// ---------we can store multiple data  in one variable OR in single ARRAY in JS like STRING ,NUMBER ,FLOAT---------
+// let mix=["haris","majeed","saif",12,23, 5.5,7.5]
+
+// ---------ARRAYs are mutable (we can change the values)-------------------------
+// let mutable=["haris","majeed","saif",12,23, 5.5,7.5]
+// mutable[0]="kaif";
+
+
+
+// ----------------------------------------ARRAY METHOD--------------------------------------------
+
+//------- ------Array push add new element at the end of an array --------------------------
+// let name =["haris","majeed","saif"];
+// stu.push("waris");
+
+// ------------Array pop remove the last element-----------------------------
+// let mane =["haris","majeed","saif"];
+// stu.pop();
+
+// -----------Array unshift use for adds new elements to the begining of an arry -----------------------------
+// let nam =["haris","majeed","saif"];
+// nam.unshift("waris");
+
+// ----------------Array shift use for  delete elements begining of an arry 
+// let nam =["haris","majeed","saif"];
+// nam.shift();
+
+
+// ------------------------------------------------indexOf
+// let index=["haeis","waris","majeed","saif"]
+// index.indexOf("haris")
+
+// --------------------------------------------------includes TRUE OR FALSE
+// let indexx=["haeis","waris","majeed","saif"]
+// indexx.includes("haris")
+
+//---------------------------------------------------- mearge
+// let indeii=["haeis","waris","majeed","saif"]
+// let index=["kaif","fahman","abdul","nawaf"]
+// console.log=indeii.concat(index);
+
+// ----------------------------------------------reverse
+// let index=["kaif","fahman","abdul","nawaf"]
+// index.reverse();
+
+// ------------------------------------slice
+// let color2=["red","blue","pink","white","green",];
+// color2.slice();
+// color2.slice(2);
+// color2.slice(2,0);
+// color2.slice(-2);
+
+// --------------------------------splice
+// let color=["red","blue","pink","white","green",];
+// color.splice(1,0,"green");
+// color.splice(1,1,"green");
+// color.splice(2);
+
+// ---------------------------------------loops------------------------------------
+
+//------------------------- for loop
+// for (let i=0; i<=5; i++){
+//     console.log(i)
+// }
+//------------- even Number
+// for (let i=0; i<=20; i=i+2) {
+//     console.log(i)
+// }
+
+// --------------------odd Number
+// for (let i=1; i<=20; i=i+2) {
+//     console.log(i)
+// }
+
+// -----------------------------infinite loop
+// for (let i=0; i>=0; i++){
+//     console.log(i)
+// }
+
+// for (let i=0; ; i++){
+//     console.log(i)
+// }
+
+// -----------------------------nested loop
+// for (let i=0; i<=3; i++){
+//     console.log(`outer: ${i}`);
+//     for (let j=0; j<=5; j++){
+//     console.log(j)    
+// } 
+// } 
+
+// while loop
+// let i=1;
+// while (i<=11) {
+//     if(i==5){
+//         break;
+//     }
+//     console.log(i);
+//     i++;
+// }
+
+// odd Number
+// let i=1;
+// while (i<=20) {
+//     console.log(i);
+//     i=i+2;
+// }
+
+// even Number
+// let j=0;
+// while (j<=20) {
+//     console.log(j);
+//     j=j+2;
+// }
+
+// let hero=[
+//     ["haris","waris","majeed"],
+//     ["saris","raris","sada"]
+// ];
+// for(let j=0; j<hero.length; j++);{
+//     console.log(hero[j]);
+// }
+
+// let naw=[["haris",56],["majeed",79],["waris",42],["sada",46],["badul",90],["saif",12]]
+
+// for(let i=0; i<naw.length; i++);
+// {
+//     console.log(`info #${i+1}`)
+//     for(let j=0; j<naw[i].length; j++){
+//         console.log(naw[i][j]);
+//     }
+// }
+
+
+// let fruits=["apple", "orange", "green","yellow","orange"]
+// for(fruit of fruits){
+//     console.log(fruit)
+// }
+
+// for(har of "mohammehharis"){
+//     console.log(har)
+// }
+
+// let frs=[["apple", "orange", "green"],["yellow","orange","red","percent"]];
+// for(fird of frs){
+//     for(ries of fird){
+//         console.log(ries)
+//     } 
+// }
+
+// let arr=[1,2,3,4,5,6,2,3];
+// let num=2;
+// for(let i=0;i<arr.length;i++){ 
+//     if(arr[i]==num)
+//     {
+//         arr.splice(i,1);
+//     }
+// }
+// console.log(arr);
+
+
+// let number=287152;
+// let count=0;
+
+// let copy=number; 
+// while(copy>0)
+// {
+//     count++;
+//     copy=Math.floor(copy/10);
+// }
+// console.log(copy);
+
+// let number=287152;
+// let sum=0;
+// let copy=number;
+
+// while(copy>0)
+// {
+//     digit=copy%10;
+//     sum+=digit;
+//     copy=Math.floor(copy/10);
+// }
+// console.log(sum);
+
+// let n=5;
+// let factorial=1;
+// for(let i=1; i<=n; i++)
+// {
+//     factorial*=i;
+// }
+// console.log(`factorial of ${n} is ${factorial}`)
+
+
+// let arr=[2,5,10,4,2,7,1,9];
+// let largest=0;
+// for(let i=0; i<=arr.length; i++)
+// {
+//     if(largest<arr[i])
+//     {
+//         largest=arr[i];
+//     }
+// }
+// console.log(largest);
+
+
+// for (let i=0; i<12; i++){
+//     for (let j=0; j<12; j++);{
+//         console.log(`${i} x ${j} = ${i*j}`);
+//     }
+// }
+// console.log("haris")
+
+// const studee ={
+//     1: 2,
+//     name: 'haris',
+//     class: "final years",
+//     marks: 452,
+//     addres: "bunder road",
+//     father:["hussain","mohammed"]
+// }
+
+
+// const infostud = {
+//     haris:{
+//         class: 3,
+//         grade:"first"
+//     },
+//     majeed:{
+//         class: "first",
+//         grade:"second"
+//     },
+//     saif:{
+//         class: "two",
+//         grade:"third"
+//     }
+
+// }
+
+// const classio = [
+//     {
+//         name:"haris",
+//         class: "three",
+//         grade:"second"
+//     },
+//     {
+//         name:"waris",
+//         point:98.9,
+//         class: "four"
+//     },
+//     {
+//         name:"abdull",
+//         city:"dubai",
+//         class: "five"
+//     },
+
+// ]
+
+
+// const max = prompt("enter the Max number");
+
+// const random = Math.floor(Math.random()* max) + 1 ;
+// console.log(random) ;
+
+// let guess = prompt("guess the number");
+
+// let dice=Math.floor(Math.random()*6)+1; console.log(dice);
+
+// const car = {
+//     name: "Maruti Suzuki", 
+//     model: "Maruti Suzuki Dzire",
+//     color: "white",
+// };
+
+// console.log(car.name);
+
+// const Person = {
+//     name:"Jane Doe",
+//     age:21,
+//     city:"New Jersey",
+// };
+// Person.city="NewYork";
+// Person.country="United States";
+// console.log(Person);
+
+
+// function helloWorld() {
+//     console.log("how are you?");
+// }
+
+// helloWorld();
+
+// function print1to10(){
+//     for(let i=1; i<10; i++){
+//         console.log(i);
+//     }
+// }
+// print1to10();
+
+
+// function adult1(){
+//     let age= 19;
+//     if (age >= 18){
+//         console.log ("adult");
+//     }
+//     else {
+//         console.log("not adult")
+//     }
+// }
+
+// adult1();
+
+
+
+// function printname(name,age) {
+//     console.log (`name: ${name} age is : ${age}`);
+// }
+// printname("haris",23);
+
+
+// function sum(a,b){
+//     console.log (a+b);
+// }
+
+// sum(2,4)
+
+
+
+// function cult(a,b,c){
+//     let avg = (a+b+c)/2;
+//     console.log (avg);
+// }
+// cult(2,3,3);
+
+// function printIN(name,age) {
+//      console.log (`name: ${name} age is : ${age}`);
+//  }
+ 
+// printIN(77);
+
+
+
+
+// function getSum(n){
+//     let sum = 0;
+
+
+//     for(let i=0; i<n; i++){
+//         sum+=i
+//     }
+//     return sum;
+// }
+
+// {
+//     let a = 4;
+//     console.log(a);
+// }
+
+
+
+// const person = {
+//     name : 'John',
+//     age : 34,
+//     city : 'San Francisco',
+// }
+// const{ name, age, city } = person;
+
+// const person2 = {...person, name: "jack"}
+
+// const name2 = ["haris", "jack ","mosquito"];
+// const name3 = [...name2,"morris"]
+
+
+// Map() function-----------------------------
+
+// let names = ["jack ","mosquito"];
+
+// names.filter((name) => {
+//     return name !== "haris"
+// })
+
+
+// const cube = (n) => (n * n * n);
+
+
+let para1 = document.createElement("p");
+para1.innertext = "hey i'm red";
+document.querySelector("body").append(para1)
---
+++ parctice.js
@@ -0,0 +1,3 @@
+let para1 = document.createElement("p");
+para1.innertext = "hey i'm red";
+document.querySelector("body").append(para1)
